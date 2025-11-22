289144f0a5f9547748f2466511bddccf9257cb2c
lwonsang <l.wonsang@wustl.edu>
2025-11-22 15:53:50 -0600

    Initial Commit

---
+++ education-accessibility-dyslexia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  education-accessibility-dyslexia
+//
+//  Created by Wonsang Lee on 11/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ education-accessibility-dyslexia/education_accessibility_dyslexiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  education_accessibility_dyslexiaApp.swift
+//  education-accessibility-dyslexia
+//
+//  Created by Wonsang Lee on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct education_accessibility_dyslexiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
